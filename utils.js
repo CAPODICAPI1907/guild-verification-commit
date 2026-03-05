6e656a4fdd14f92c1cb7127ef11fc1efe0d8c052
CAPODICAPI1907 <99887412+CAPODICAPI1907@users.noreply.github.com>
2026-03-06 01:46:05 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:45:50.193Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-05T22:46:03.210Z
+Commit: Add export functionality for reports
