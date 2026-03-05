b55f4ae24a57cae8c735a2ed7815bbc259858881
CAPODICAPI1907 <99887412+CAPODICAPI1907@users.noreply.github.com>
2026-03-06 01:45:37 +0300

    Fix broken links in documentation

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:44:07.284Z
-Commit: Implement user profile customization
+Timestamp: 2026-03-05T22:45:35.438Z
+Commit: Fix broken links in documentation
