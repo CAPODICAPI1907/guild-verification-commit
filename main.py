2d03982fb1643e46d7bd7007cced01f9c14b61a7
CAPODICAPI1907 <99887412+CAPODICAPI1907@users.noreply.github.com>
2026-03-06 01:51:06 +0300

    Patch API endpoint error handling

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:51:02.833Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T22:51:04.534Z
+Commit: Patch API endpoint error handling
