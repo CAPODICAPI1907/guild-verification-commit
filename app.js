e80ff01e550a3c319117c818721b5f4515aed097
CAPODICAPI1907 <99887412+CAPODICAPI1907@users.noreply.github.com>
2026-03-06 01:45:55 +0300

    Resolve null pointer exception in user service

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:45:46.625Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-05T22:45:53.582Z
+Commit: Resolve null pointer exception in user service
