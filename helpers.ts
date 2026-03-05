25b1c4cb01e7c52463030e2cf6fa5b228306af2c
CAPODICAPI1907 <99887412+CAPODICAPI1907@users.noreply.github.com>
2026-03-06 01:49:55 +0300

    Implement user profile customization

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:49:46.280Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-05T22:49:53.513Z
+Commit: Implement user profile customization
