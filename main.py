0c0ef75f8a70fbe01515f1bd4e19b148bf335ba6
CAPODICAPI1907 <99887412+CAPODICAPI1907@users.noreply.github.com>
2026-03-06 01:51:05 +0300

    Fix broken links in documentation

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-05T22:51:01.289Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-05T22:51:02.833Z
+Commit: Fix broken links in documentation
